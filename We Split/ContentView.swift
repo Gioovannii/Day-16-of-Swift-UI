0de758a02052e3631b55142c25368517b64d3ff6
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-13 16:21:46 +0800

    Initial Commit

---
+++ We Split/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  We Split
+//
+//  Created by Giovanni Gaffé on 2021/9/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ We Split/We_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_SplitApp.swift
+//  We Split
+//
+//  Created by Giovanni Gaffé on 2021/9/13.
+//
+
+import SwiftUI
+
+@main
+struct We_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
