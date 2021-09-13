48aff744d6ecdaf88641e9eee4cd70e6f84bb36a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-13 18:12:03 +0800

    change tap count to @state

--- We Split/ContentView.swift
+++ We Split/ContentView.swift
@@ -8,14 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello, world!")
-                }
-            }
-            .navigationBarTitle("Swift UI")
+        Button("tap Count \(tapCount)") {
+            self.tapCount += 1
         }
     }
 }
