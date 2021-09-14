a49ab5c4ed42511d59c29eacf786a6dca387059d
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-14 14:51:58 +0800

    calculate what every one need to pay

--- We Split/ContentView.swift
+++ We Split/ContentView.swift
@@ -15,6 +15,20 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -28,11 +42,21 @@ struct ContentView: View {
                         }
                     }
                 }
+                
+                Section(header: Text("How much tip do you want to leave ?")) {
+                    Picker("tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0]) %")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
                 Section {
-                    Text("$\(checkAmount)")
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
-        .navigationBarTitle("We Split")
+            .navigationBarTitle("We Split")
         }
     }
 }
