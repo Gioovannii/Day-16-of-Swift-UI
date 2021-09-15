1206d243b850fa2ad8967a0b39fc02c166b0d7fb
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-15 13:42:37 +0800

    challenge

--- We Split/ContentView.swift
+++ We Split/ContentView.swift
@@ -15,58 +15,58 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople)
+        let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
-        
+
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
-        
+
         return amountPerPerson
     }
     
-    var totalAmountPlustips: Double {
-        let peopleCount = Double(numberOfPeople)
-        let total = totalPerPerson * peopleCount
-        return total
+    var totalBill: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+
+        let total = totalPerPerson
+        return total * peopleCount
     }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amount", text: $checkAmount)
+                    TextField("Montant", text: $checkAmount)
                         .keyboardType(.decimalPad)
                     
-                    TextField("Number of people", text: .constant("\(numberOfPeople)"))
-//                    Picker("Number of people", selection: $numberOfPeople) {
-//                        ForEach(2 ..< 100) {
-//                            Text("\($0) people")
-//                        }
-//                    }
+                    Picker("Nombre de personnes", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) personnes")
+                        }
+                    }
                 }
                 
-                Section(header: Text("How much tip do you want to leave ?")) {
-                    Picker("tip percentage", selection: $tipPercentage) {
+                Section(header: Text("Combien de pourboire voulez-vous laissez ?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0 ..< tipPercentages.count) {
-                            Text("\(self.tipPercentages[$0]) %")
+                            Text("\(self.tipPercentages[$0])%")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section(header: Text("Amount per person")) {
+                Section(header: Text("Montant par personne")) {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
                 
-                Section(header: Text("Original bill")) {
-                    Text("Total amount \(totalAmountPlustips, specifier: "%.2f")")
+                Section(header: Text("Facture totale avec pourboire")) {
+                    Text("$\(totalBill, specifier: "%.2f")")
+                    
                 }
             }
-            .navigationBarTitle("We Split")
+            .navigationBarTitle("On partage")
         }
     }
 }
